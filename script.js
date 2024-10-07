6bcd1e0304706adff24f541a80f3c41d5c7de003
Dominika Pirosova <pirosova.dominika@gmail.com>
2024-10-07 19:31:59 +0200

    commit--2

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 /* Dva spôsoby zapisovania*/
 
-const nazev = "Kniha 1"
-const autor = "Michal Kucera"
+const nazov = "Knižka"
+const autor = "Dominika Pirošová"
 const cena = 450
 const naSklade = true
 
 const kniha = {
-    nazev: "Kniha 1",
-    autor: "Michal Kucera",
+    nazov: "Knižka",
+    autor: "Dominika Pirošová",
     cena: 450,
     naSklade: true,
 }
 
-document.body.innerHTML += "<p>Kniha: " + nazev + ", autor: " + autor + "</p>"
-document.body.innerHTML += "<p>Kniha: " + kniha.nazev + ", autor: " + kniha.autor + "</p>"
+document.body.innerHTML += "<p>Kniha: " + nazov + ", autor: " + autor + "</p>"
+document.body.innerHTML += "<p>Kniha: " + kniha.nazov + ", autor: " + kniha.autor + "</p>"
